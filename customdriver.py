d796abc68eeba08c99f1efc6532af0c1795dea78
Oleg Port <oleg.portnikhin@eteron.ru>
2019-03-12 15:14:14 +0300

    FirefoxProfileDriver Refactoring + profile_path as mandatory field.

--- customdriver.py
+++ customdriver.py
@@ -6,12 +6,11 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class FirefoxProfileDriver(WebDriver):
-    profile_path = None
-    before_temp_dirs = None
-    TMPDIR = '/tmp'
 
     def __init__(self, *args, **kwargs):
-        self.profile_path = kwargs.get('profile_path')
+        self.profile_path = kwargs['profile_path']
+        if not os.path.isdir(self.profile_path):
+            os.makedirs(self.profile_path)
         if self.profile_path:
             firefox_profile = webdriver.FirefoxProfile(self.profile_path)
             kwargs['firefox_profile'] = firefox_profile
@@ -22,7 +21,6 @@ class FirefoxProfileDriver(WebDriver):
 
         rust_profile_name = self.capabilities['moz:profile']
         shutil.rmtree(self.profile_path)
-
-        shutil.copytree(os.path.join(self.TMPDIR, rust_profile_name), self.profile_path,
+        shutil.copytree(rust_profile_name, self.profile_path,
                         ignore=shutil.ignore_patterns("parent.lock", "lock", ".parentlock"))
         super(FirefoxProfileDriver, self).quit()
